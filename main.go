f13ebbc7da85700ade71f60cad87c8baf1fd9fb1
1148973713 <1148973713@qq.com>
2021-07-26 14:00:58 +0800

    实现文件下载

--- main.go
+++ main.go
@@ -6,12 +6,13 @@ import (
 	"net/http"
 )
 
-func main()  {
-	http.HandleFunc("/file/upload",handler.UploadHandler)
-	http.HandleFunc("/file/upload/suc",handler.UploadSucHandler)
-	http.HandleFunc("/file/meta",handler.FileQueryHandler)
+func main() {
+	http.HandleFunc("/file/upload", handler.UploadHandler)
+	http.HandleFunc("/file/upload/suc", handler.UploadSucHandler)
+	http.HandleFunc("/file/meta", handler.FileQueryHandler)
+	http.HandleFunc("/file/download", handler.DownloadHandler)
 	err := http.ListenAndServe(":8080", nil)
-	if err!=nil{
-		fmt.Print("Failed to start server,err:%s",err.Error())
+	if err != nil {
+		fmt.Print("Failed to start server,err:%s", err.Error())
 	}
 }
