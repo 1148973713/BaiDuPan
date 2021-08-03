8f465dd564da1653b7bccccf2a2d6c16ebea6dba
1148973713 <1148973713@qq.com>
2021-08-03 17:11:43 +0800

    mysql用户登录注册

--- db/user.go
+++ db/user.go
@@ -7,7 +7,7 @@ import (
 
 //注册操作
 func UserSignup(username string, password string) bool {
-	stmt, err := mysql.DBCoon().Prepare("inster ignore into tbl_user(`user_name`,`user_pwd`)value (?,?)")
+	stmt, err := mysql.DBCoon().Prepare("insert ignore into tbl_user(`user_name`,`user_pwd`)value (?,?)")
 	if err != nil {
 		fmt.Print("Failed to instert,err" + err.Error())
 		return false
@@ -22,4 +22,22 @@ func UserSignup(username string, password string) bool {
 		return true
 	}
 	return false
+}
+
+func UserSignIn(username string, encpwd string) bool {
+	stmt, err := mysql.DBCoon().Prepare("select * from tbl_user where user_name=?limit1")
+	if err != nil {
+		fmt.Printf(err.Error())
+		return false
+	}
+	row, err := stmt.Query(username)
+	if err != nil {
+		fmt.Printf(err.Error())
+		return false
+	} else if row == nil {
+		fmt.Printf("user not find:" + username)
+		return false
+	}
+	defer stmt.Close()
+	pProws := mysql.ParseRows(row)
 }
--- handler/user.go
+++ handler/user.go
@@ -23,7 +23,7 @@ func SignupHandler(w http.ResponseWriter, r *http.Request) {
 	username := r.Form.Get("username")
 	password := r.Form.Get("password")
 
-	if len(username) > 3 || len(password) < 5 {
+	if len(username) < 3 || len(password) < 5 {
 		w.Write([]byte("Invalid parameter"))
 		return
 	}
@@ -31,8 +31,16 @@ func SignupHandler(w http.ResponseWriter, r *http.Request) {
 	enc_password := util.Sha1([]byte(password + pwd_salt))
 	suc := db.UserSignup(username, enc_password)
 	if suc {
-		w.Write([]byte("success"))
+		w.Write([]byte("SUCCESS"))
 	} else {
-		w.Write([]byte("warning"))
+		w.Write([]byte("WARNING"))
 	}
+}
+
+func SignInHandler(w http.ResponseWriter, r *http.Request) {
+	//1、用户校验
+
+	//2、生产方位你凭证token
+
+	//3、重定向到首页
 }
