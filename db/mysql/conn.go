576a842b59f745c8d78d4e10b50081c1d5026b52
1148973713 <1148973713@qq.com>
2021-08-11 17:32:40 +0800

    mysql用户登录注册

--- db/mysql/conn.go
+++ db/mysql/conn.go
@@ -26,11 +26,15 @@ func DBCoon() *sql.DB {
 }
 
 func ParseRows(rows *sql.Rows) []map[string]interface{} {
-	//返回列名。
+	//返回列名
 	columns, _ := rows.Columns()
+	//https://www.cnblogs.com/echojson/p/10746807.html
 	scanArgs := make([]interface{}, len(columns))
 	values := make([]interface{}, len(columns))
+	//构造scanArgs、values两个数组，scanArgs的每个值指向values相应值的地址
 	for j := range values {
+		//&符号的意思是对变量取地址，如：变量a的地址是&a
+		//*符号的意思是对指针取值，如:*&a，就是a变量所在地址的值，当然也就是a的值了
 		scanArgs[j] = &values[j]
 	}
 
@@ -38,16 +42,21 @@ func ParseRows(rows *sql.Rows) []map[string]interface{} {
 	records := make([]map[string]interface{}, 0)
 	for rows.Next() {
 		//将行数据保存到record字典
+		//rows.Scan自动赋值
 		err := rows.Scan(scanArgs...)
 		checkErr(err)
 
+		//便利values数组，如果不为空，则将index传入columns,record[columns[i]]实现了列名与数值相对应：password - 123456  7891011
 		for i, col := range values {
 			if col != nil {
 				record[columns[i]] = col
 			}
 		}
+		fmt.Println(record)
+		//形成数组
 		records = append(records, record)
 	}
+
 	return records
 }
 
